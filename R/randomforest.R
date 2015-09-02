d2838397b7568c040fcebcf4fcaa39bd6daa9eb9
tasukujp <tasuku.kakimoto@gmail.com>
2015-09-02 23:43:33 +0900

    first commit

---
+++ R/randomforest.R
@@ -0,0 +1,45 @@
+library(data.table)
+library(ggplot2)
+library(randomForest)
+
+set.seed(123)
+train <- fread("../data/train.csv")
+test <- fread("../data/test.csv")
+
+extractFeatures <- function(data) {
+  features <- c("Pclass",
+                "Sex",
+                "Age",
+                "SibSp",
+                "Parch",
+                "Fare",
+                "Cabin",
+                "Embarked")
+  fea <- data[,features, with=FALSE]
+  fea$Age[is.na(fea$Age)] <- -1
+  fea$Fare[is.na(fea$Fare)] <- median(fea$Fare, na.rm=TRUE)
+  fea$Embarked[fea$Embarked==""] = "S"
+  fea$Sex      <- as.factor(fea$Sex)
+  fea$Embarked <- as.factor(fea$Embarked)
+  return(fea)
+}
+
+rf <- randomForest(extractFeatures(train), as.factor(train$Survived), ntree=100, importance=TRUE)
+
+submission <- data.frame(PassengerId = test$PassengerId)
+submission$Survived <- predict(rf, extractFeatures(test))
+write.csv(submission, file = "../data/1_random_forest_r_submission.csv", row.names=FALSE)
+
+imp <- importance(rf, type=1)
+featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+
+p <- ggplot(featureImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+  geom_bar(stat="identity", fill="#53cfff") +
+  coord_flip() + 
+  theme_light(base_size=20) +
+  xlab("") +
+  ylab("Importance") + 
+  ggtitle("Random Forest Feature Importance\n") +
+  theme(plot.title=element_text(size=18))
+
+ggsave("../data/2_feature_importance.png", p)
